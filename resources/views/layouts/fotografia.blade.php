1700d9c2cf60e7c0cb54a1a5a913f76528e47cfd
RTAndrew <anderson_rodax@hotmail.com>
2020-02-29 12:32:15 +0100

    Add fixed width

--- resources/views/layouts/fotografia.blade.php
+++ resources/views/layouts/fotografia.blade.php
@@ -13,21 +13,29 @@
 			@include('pages.fotografia.side-nav')
 		@endsection
 	
-
-	</div>
-
-
-	<div class="foto-page">
-		
-
-			@yield('content')
-						
+		<div class="foto-page">
 			
+				@yield('content')
+							
+		</div>
+
 	</div>
 
 
+	
 
-
+	
+	
+	@section('script-full')
+		{{-- Add class tag 
+			on the Sidenav so the width variation
+			doesnt affect the /textos
+		--}}
+		<script>
+			var sideNav = document.querySelector('.pushbar');
+			sideNav.classList.add("fotografia-sideBar");
+		</script>
+	@endsection
 
 @endsection
 
